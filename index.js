55130d9f2319d666fff31de5f807722f13c6a37e
hanswatkins <hanswatkins@gmail.com>
2022-08-04 19:13:25 -0700

    Created all routes in restaurantsController
    Co-Authored-By: Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 // MAKE SURE YOU HAVE RUN npm i express mongoose dotenv
 // Basic config
 
-const express = require('express')
-const app = express()
-require('dotenv').config()
-app.set('port', process.env.PORT || 8000)
-const cors = require('cors')
+const express = require('express');
+const app = express();
+require('dotenv').config();
+app.set('port', process.env.PORT || 8000);
+const cors = require('cors');
 
 // Middleware
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // ROUTES
 /// Redirect
 app.get('/', (req, res) => {
-    res.redirect('/restaurants')
-})
+  res.redirect('/restaurants');
+});
 
 /// Controllers
 
-const restaurantsController = require('./controllers/restaurantsController')
-app.use('/restaurants', restaurantsController)
+const restaurantsController = require('./controllers/restaurantsController');
+app.use('/restaurants', restaurantsController);
 
-const messagesController = require('./controllers/messagesController')
-app.use('/messages', messagesController)
+const messagesController = require('./controllers/messagesController');
+app.use('/messages', messagesController);
 
-const reviewsController = require('./controllers/reviewsController')
-app.use('/reviews', reviewsController)
+const reviewsController = require('./controllers/reviewsController');
+app.use('/reviews', reviewsController);
 
-const usersController = require('./controllers/usersController')
-app.use('/users', usersController)
+const usersController = require('./controllers/usersController');
+app.use('/users', usersController);
 
-// Error handle 
+// Error handle
 app.use((err, req, res, next) => {
-    const statusCode = res.statusCode || 500
-    const message = err.message || 'Internal Server Error'
-    res.status(statusCode).send(message)
-})
+  const statusCode = res.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).send(message);
+});
 
 // Start server
 app.listen(app.get('port'), () => {
-    console.log(`✅ PORT: ${app.get('port')} 🌟`)
-})
+  console.log(`✅ PORT: ${app.get('port')} 🌟`);
+});
