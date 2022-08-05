8dd0e044a35ffd72b0ca244b729f20c5b52fc197
hanswatkins <hanswatkins@gmail.com>
2022-08-04 20:33:19 -0700

    Completed routes in usersController and reviewsController
    Co-Authored-By: Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
    Co-Authored-By: David Sandoval <82617473+DavidJoao@users.noreply.github.com>
    Co-Authored-By: Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,59 @@
 const express = require('express');
+const router = express.Router();
 const User = require('../models/user')
 
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    User.find({}).then((users) => {
+router.get('/', async (req, res, next) => {
+    try {
+        const users = await User.find({})
         res.json(users)
-    })
+    } catch(err) {
+        next(err)
+    }
 })
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.json(user)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const newUser = await User.create(req.body)
+        res.json(newUser)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        if (updatedItem) {
+            res.json(updatedUser)
+        } else {
+            res.sendStatus(404)
+        }
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try{
+        const deletedUser = await User.findByIdAndDelete(req.params.id)
+        res.json(deletedUser)
+    } catch(err) {
+        next(err)
+    }
+})
+
+
+module.exports = router;
+
+
+
+
