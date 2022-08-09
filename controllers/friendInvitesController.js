6b556667375b4a01930c5ee2e8fa8663e8aa5c65
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-08-09 09:57:01 -0700

    Updates wording in friendinvite routes for readability
    Co-Authored-By: Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>

--- controllers/friendInvitesController.js
+++ controllers/friendInvitesController.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const User = require('../models/user')
 
 // Add friend invite
-// PUT /friendinvites/:userId
-router.put('/:userId', (req, res, next) => {
-   User.findById(req.params.userId)
+// PUT /friendinvites/:recipientId
+router.put('/:recipientId', (req, res, next) => {
+   User.findById(req.params.recipientId)
        .then(user => {
            user.friendinvites.push(req.body)
            user.save()
@@ -15,9 +15,9 @@ router.put('/:userId', (req, res, next) => {
 })
 
 // Delete friend invite
-// DELETE /friendinvites/:inviteId/user/:userId
-router.delete('/:senderId/user/:userId', (req, res, next) => {
-   User.findByIdAndUpdate(req.params.userId, { $pull: { friendinvites: { sender: req.params.senderId}}}, { new: true })
+// DELETE /friendinvites/:inviteId/user/:recipientId
+router.delete('/:inviteId/user/:recipientId', (req, res, next) => {
+   User.findByIdAndUpdate(req.params.recipientId, { $pull: { friendinvites: { _id: req.params.inviteId}}}, { new: true })
       .then(user => res.json(user))
       .catch(next)
 })
